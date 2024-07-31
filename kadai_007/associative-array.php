791b6b9c36fd71d276a65469700684dee9524830
teoria-kimura <162103512+teoria-kimura@users.noreply.github.com>
2024-07-31 12:27:58 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>課題007</title>
+  </head>
+  <body>
+    <p>
+      <?php 
+      $product_details = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+      print_r($product_details);
+
+      ?>
+    </p>
+  </body>
+</html>
